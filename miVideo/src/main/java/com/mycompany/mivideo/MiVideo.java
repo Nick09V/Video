f2274ba42b1fd8385f91cd158815a862eb90024c
Nick <nick.valverde@epn.edu.ec>
2024-07-17 20:13:03 -0500

    Primer Commit

---
+++ miVideo/src/main/java/com/mycompany/mivideo/MiVideo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mivideo;
+
+/**
+ *
+ * @author nickv
+ */
+public class MiVideo {
+
+    public static void main(String[] args) {
+        System.out.println(" Buenas tardes");
+    }
+}
